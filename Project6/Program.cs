2528cc5561b8585a75a1fa78a946a117e2d0c921
NikolayKartaev <kartaevgeek@gmail.com>
2023-01-26 07:22:32 +0300

    Add Project6 - Comparing numbers

---
+++ Project6/Program.cs
@@ -0,0 +1,16 @@
+﻿// Пишем шестую программу на C#: "Сравниваем числа"
+int a = 1;
+int b = 5;
+int c = 10;
+int d = 9;
+int e = 2;
+
+int max = a;
+
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+if (d>max) max = d;
+if (e>max) max = e;
+Console.Write("max = ");
+Console.WriteLine(max);
