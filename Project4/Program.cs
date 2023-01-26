0b0edde0864e5c8f7cd189a197093000977749ea
NikolayKartaev <kartaevgeek@gmail.com>
2023-01-26 06:57:20 +0300

    Add Project4 - Added division of numbers

---
+++ Project4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Пишем четвертую программу на C#: "Деление двух чисел"
+double numberA = new Random().Next(1,100001);
+Console.Write("Первое число: ");
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1,11);
+Console.Write("Второе число: ");
+Console.WriteLine(numberB);
+Console.Write("Частное двух числе равно: ");
+double div = numberA/numberB;
+Console.WriteLine(div);
