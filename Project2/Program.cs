4f420ae5543b9c28577647e8be0ecbd663f8a70c
NikolayKartaev <kartaevgeek@gmail.com>
2023-01-26 06:31:13 +0300

    Add Project2 - Hello to User

---
+++ Project2/Program.cs
@@ -0,0 +1,5 @@
+﻿// Пишем вторую программу на C#: "Сказать привет пользователю"
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.WriteLine (username);
