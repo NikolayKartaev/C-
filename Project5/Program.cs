34a89637bf294c76678c7974cfe450f62414beaf
NikolayKartaev <kartaevgeek@gmail.com>
2023-01-26 07:13:22 +0300

    Add Project5 - Added Greetings to diff users

---
+++ Project5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Пишем пятую программу на C#: "Разное приветствие пользователей"
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("УРА! Это же МАША!");
+ }
+ else
+ {
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+ }
