847d74bb8e7eabcc05a191616c233090724527b3
NikolayKartaev <kartaevgeek@gmail.com>
2023-01-31 08:16:25 +0300

    Add Project11 - Find matches in Array

---
+++ Project11.Lesson2/Program.cs
@@ -0,0 +1,15 @@
+﻿int[]array={14,22,3,43,50,36,57,50};
+int n = array.Length;
+
+int find = 50;
+int index=0;
+
+while (index<n)
+{
+    if (array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index=index+1;
+}
