5be35e8baf29fc05c69bae9892af4c61b8a94366
NikolayKartaev <kartaevgeek@gmail.com>
2023-02-01 19:19:48 +0300

    Finished Project12

--- Project12.Lesson2/Program.cs
+++ Project12.Lesson2/Program.cs
@@ -1,1 +1,38 @@
-﻿int[] array = new int [10]; // создать новый массив с 10 элементами
+﻿void FillArray (int [] Collection)
+{
+    int index = 0;
+    int length = Collection.Length;
+    while (index < length)
+    {
+        Collection[index]=new Random().Next(1,11);
+        index++; //index=index+1
+    }
+}
+void PrintArray (int [] Col)
+{
+    int count = 0;
+    int number = Col.Length;
+    while (count<number)
+    {
+        Console.Write($"{Col[count]} ");
+        count++;
+    }
+}
+
+int[] array = new int [5]; // создать новый массив с 10 элементами
+
+FillArray (array);
+PrintArray (array);
+
+void ChangeOnMinus (int [] array2)
+{
+    int m =0;
+    while (m<array2.Length)
+    {
+        array2[m]=array2[m]*(-1);
+        m++;
+    }
+}
+Console.WriteLine();
+ChangeOnMinus(array);
+PrintArray (array);
