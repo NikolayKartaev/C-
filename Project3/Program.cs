2d655a8bffe891178553a8de99cd4b9f7ba8b538
NikolayKartaev <kartaevgeek@gmail.com>
2023-02-03 07:44:08 +0300

    Latest fixes

--- Project3/Program.cs
+++ Project3/Program.cs
@@ -1,5 +1,8 @@
 ﻿// Пишем третью программу на C#: "Сложение двух чисел"
-int numberA = 10;
-int numberB = 25;
+Console.Write("Введите 1-ое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
 int result = numberA+numberB;
-Console.WriteLine(result);
+
+Console.Write($"Сумма чисел {numberA} и {numberB} равна {numberA+numberB}");
--- Project9.Lesson2/Program.cs
+++ Project9.Lesson2/Program.cs
@@ -1,27 +1,27 @@
-﻿int Max (int arg1, int arg2, int arg3)
+﻿int FindMax (int a, int b, int c)
 {
-    int result = arg1;
-    if (arg2>result) result=arg2;
-    if (arg3>result) result=arg3;
-    return result;
+    int Max=a;
+    if (b>Max) Max=b;
+    if (c>Max) Max=c;
+    return Max;
 }
-int a1 =10;
-int b1 =20;
-int c1 =3000;
+int a1=1;
+int a2=2;
+int a3=300;
 
-int a2 =430;
-int b2 =50;
-int c2 =60;
+int b1=10;
+int b2=200;
+int b3=30;
 
-int a3 =17770;
-int b3 =80;
-int c3 =10000;
+int c1=500;
+int c2=150;
+int c3=100;
 
-// int max1 = Max (a1,b1,c1);
-// int max2 = Max (a2,b2,c2);
-// int max3 = Max (a3,b3,c3);
-// int maximum = Max(max1,max2,max3);
-
-int maximum = Max(Max (a1,b1,c1),Max (a2,b2,c2),Max (a3,b3,c3));
-
-Console.WriteLine (maximum);
+int max1= FindMax (a1,a2,a3);
+Console.WriteLine($"Максимум из чисел {a1}, {a2}, {a3} = {max1}");
+int max2= FindMax (b1,b2,b3);
+Console.WriteLine($"Максимум из чисел {b1}, {b2}, {b3} = {max2}");
+int max3= FindMax (c1,c2,c3);
+Console.WriteLine($"Максимум из чисел {c1}, {c2}, {c3} = {max3}");
+int maxAll= FindMax (max1,max2,max3);
+Console.Write($"Максимум из максимумов = {maxAll}");
