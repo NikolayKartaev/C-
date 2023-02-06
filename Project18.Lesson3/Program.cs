59a339765feacb50fb9c403e2de5986eaa2c23a7
NikolayKartaev <kartaevgeek@gmail.com>
2023-02-06 08:00:49 +0300

    Цикл for, цикл в цикле

---
+++ Project18.Lesson3/Program.cs
@@ -0,0 +1,11 @@
+﻿string Method4(int count, string msg)
+{
+    string res = String.Empty;
+    for (int i = 0; i < count; i++) // цикл for вместо while
+    {
+        res = res + msg;
+    }
+    return res;
+}
+string result = Method4(msg: "-Текст Метода 4- ", count: 4);
+Console.WriteLine(result);
