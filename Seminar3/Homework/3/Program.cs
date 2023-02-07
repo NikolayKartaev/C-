f13ce76d78c535ef96b64b52be07a0d83d4dd4d7
NikolayKartaev <kartaevgeek@gmail.com>
2023-02-07 14:15:43 +0300

    Seminar 3 Homework 1-4

--- Seminar3/Homework/3/Program.cs
+++ Seminar3/Homework/3/Program.cs
@@ -1,7 +1,6 @@
-﻿// Задача 23
-
+﻿// Seminar 3 Homework.Number 3
+// Задача 23
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
@@ -12,5 +11,5 @@ Console.Write($"Таблица кубов чисел от 1 до {N}: ");
 for (int i = 1; i <= N; i++)
 {
     if (i<N) Console.Write($"{Math.Pow(i, 3)}, ");
-    else Console.Write($"{Math.Pow(i, 3)} ");
+    else Console.Write($"{Math.Pow(i, 3)}");
 }
--- Seminar3/Homework/4/Program.cs
+++ Seminar3/Homework/4/Program.cs
@@ -1,26 +1,11 @@
-﻿// Сбор черники
-
-// В фермерском хозяйстве в Карелии выращивают чернику. Она растет на круглой грядке, причем кусты высажены только по окружности. // Таким образом, у каждого куста есть ровно два соседних. Всего на грядке растет N кустов. // Эти кусты обладают разной урожайностью, поэтому ко времени сбора на них выросло различное число ягод – на i-ом кусте выросло ai ягод.
-// В этом фермерском хозяйстве внедрена система автоматического сбора черники. Эта система состоит из управляющего модуля и 
-// нескольких собирающих модулей. Собирающий модуль за один заход, находясь непосредственно перед некоторым кустом, 
-// собирает ягоды с этого куста и с двух соседних с ним. // Напишите программу для нахождения максимального числа ягод, которое может собрать за один заход собирающий модуль, 
-// находясь перед некоторым кустом заданной во входном файле грядки.
-// Входные данные
-// Первая строка входного файла INPUT.TXT содержит целое число N (3 ≤ N ≤ 1000) – количество кустов черники. 
-// Вторая строка содержит N целых положительных чисел a1, a2, ..., aN – число ягод черники, растущее на соответствующем кусте. Все ai не превосходят 1000.
-
-// Примеры
-// №	INPUT.TXT	OUTPUT.TXT
-// 4
-// 1 2 3 4         	9
-
-// 3
-// 1 2 3	        6
+﻿// Seminar 3 Homework.Number 4 (дополнительная задача)
 
 Console.Clear();
+int[] bushesArray = new int[new Random().Next(3, 1001)];
 
-int bushesNumber = new Random().Next(3, 10);
-int[] bushesArray = new int[bushesNumber];
+// int[] bushesArray = { 101, 110, 3, 4, 5, 2, 4, 1, 1, 100, 34, 100}; - проверка работоспособности
+
+Console.WriteLine($"Количество кустов на грядке: {bushesArray.Length}");
 
 void FillArray(int[] arr)
 {
@@ -33,7 +18,7 @@ void PrintArray(int[] Col)
 {
     int count = 0;
     int number = Col.Length;
-    Console.Write("Массив: ");
+    Console.Write("Количества ягод на кустах: ");
     while (count < number)
     {
         Console.Write($"{Col[count]} ");
@@ -45,34 +30,29 @@ FillArray(bushesArray);
 PrintArray(bushesArray);
 
 Console.WriteLine();
-Console.WriteLine($"Количество кустов: {bushesNumber}");
 
-bushesNumber = 0;
-int MAX = 0;
-Console.WriteLine();
+int i = 0;
+int L = bushesArray.Length;
+int MaxPos = 0;
+int a = 0; int b = 0; int c = 0;
 
-int Max3Array(int[] ArrayDefault)
+while (i < L)
 {
-    int i = 0;
-    int L = ArrayDefault.Length;
-    int MaxPos = ArrayDefault[i] + ArrayDefault[i+1] + ArrayDefault[i+2];
-    while (i < L-2)
+    if ((i < L - 2) && (bushesArray[i] + bushesArray[i + 1] + bushesArray[i + 2] > MaxPos)) 
     {
-        if (ArrayDefault[i] + ArrayDefault[i+1] + ArrayDefault[i+2] > MaxPos) MaxPos = ArrayDefault[i] + ArrayDefault[i+1] + ArrayDefault[i+2];
-        i++;
+        MaxPos = bushesArray[i] + bushesArray[i + 1] + bushesArray[i + 2];
+        a = i; b = i + 1; c = i + 2;
     }
-    return MaxPos;
+    else if ((i == L - 2) && (bushesArray[i] + bushesArray[i + 1] + bushesArray[L - i - 2] > MaxPos))
+    {
+        MaxPos = bushesArray[i] + bushesArray[i + 1] + bushesArray[L - i - 2];
+        a = i; b = i + 1; c = L - i - 2;
+    }
+    else if ((i == L - 1) && (bushesArray[i] + bushesArray[L - i - 1] + bushesArray[L - i] > MaxPos))
+    {
+        MaxPos = bushesArray[i] + bushesArray[L - i - 1] + bushesArray[L - i];
+        a = i; b = L - i - 1; c = L - i;
+    }
+    i++;
 }
-
-// while (bushesNumber < bushesArray.Length)
-// {
-//     if (bushesArray[bushesNumber] + bushesArray[bushesNumber + 1] + bushesArray[bushesNumber + 2] > MAX)
-//     {
-//         MAX = bushesArray[bushesNumber] + bushesArray[bushesNumber + 1] + bushesArray[bushesNumber + 2];
-//     }
-//     bushesNumber++;
-// }
-
-MAX = Max3Array(bushesArray);
-Console.WriteLine(MAX);
-
+Console.WriteLine($"Максимальное число ягод, собранное за 1 ход с кустов №{a}, №{b}, №{c}: {MaxPos}");
