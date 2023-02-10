e78931419d806339ac087a64e24dfcd5858d9f80
NikolayKartaev <kartaevgeek@gmail.com>
2023-02-10 06:10:00 +0300

    Seminar 4 Homework Number 3

---
+++ Seminar4/Homework/3b/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+int N = new Random().Next(1, 101);
+int[] Sherenga = new int[N];
+Console.WriteLine($"Количество учеников в классе: {N}");
+Console.WriteLine();
+
+for (int i = 0; i < Sherenga.Length; i++)
+{
+    Sherenga[i] = new Random().Next(150, 201);
+}
+Console.WriteLine($"Рост учеников в классе: {String.Join(", ", Sherenga)}");
+Console.WriteLine();
+
+int PeteHeigth = new Random().Next(150, 201);
+Console.WriteLine($"Рост Пети: {PeteHeigth}");
+Console.WriteLine();
+
+int kol = 0;
+for (int s=0; s<Sherenga.Length;s++)
+{
+    if(Sherenga[s]>=PeteHeigth) kol=kol+1;
+}
+Console.WriteLine($"Позиция Пети в шеренге: {kol + 1}");
