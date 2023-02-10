c0d8fb8bdcd45c28c31c356c0ca1c1f5ade581aa
NikolayKartaev <kartaevgeek@gmail.com>
2023-02-10 06:09:24 +0300

    Seminar 4 Homework Number 2

---
+++ Seminar4/Homework/2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+string? number = Console.ReadLine();
+int sum = 0;
+if (number is not null)
+{
+    for (int i = 0; i < number.Length; i++)
+    {
+        sum = sum + Convert.ToInt32(number[i].ToString());
+    }
+}
+
+Console.WriteLine($"Сумма цифр числа {number} равно {sum}");
