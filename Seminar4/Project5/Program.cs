1de78f113437ba56a7bc8c403f36d1d71180b970
NikolayKartaev <kartaevgeek@gmail.com>
2023-02-11 11:23:29 +0300

    Seminar 4 Project 5 Modified

--- Seminar4/Project5/Program.cs
+++ Seminar4/Project5/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while (N>0)
+{
+    N=N/10;
+    count++;
+}
+Console.WriteLine(count);
+
+//Console.Write($"{Console.ReadLine().Length}");
