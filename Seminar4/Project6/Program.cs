19bacffd86f833f61c4b4d786bb7b5b1d2d4b4ff
NikolayKartaev <kartaevgeek@gmail.com>
2023-02-11 11:24:00 +0300

    Seminar 4 Project 6

---
+++ Seminar4/Project6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int result = 1;
+for (int i = 1; i<=N;i++)
+{
+    result*=i; // result = result*i
+}
+Console.WriteLine(result);
