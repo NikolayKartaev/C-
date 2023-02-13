b1b5d6e5a8e1a7947230b5c0e5365d7a2e5011e7
NikolayKartaev <kartaevgeek@gmail.com>
2023-02-13 08:09:44 +0300

    Seminar 4 Project 7

---
+++ Seminar4/Project7/Program.cs
@@ -0,0 +1,11 @@
+﻿void ArrayFill(int [] collection)
+{
+    for (int index = 0; index < collection.Length; index++)
+    {
+        collection [index] = new Random().Next(0,2);
+    }
+}
+
+int[]array= new int[8];
+ArrayFill(array);
+Console.WriteLine($"Массив: [{String.Join(",",array)}]");
