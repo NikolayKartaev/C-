17fa4c131a2ddc8cd92549ab976c6e3ba46ee86d
NikolayKartaev <kartaevgeek@gmail.com>
2023-02-13 08:11:23 +0300

    Seminar 4 Project 4

--- Seminar4/Project4/Program.cs
+++ Seminar4/Project4/Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.WriteLine("Введите число: ");
 double N = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine($"Результат сложения цифр числа: {(1+N)/2*N}");
+Console.WriteLine($"Результат сложения цифр числа: {(1 + N) / 2 * N}");
