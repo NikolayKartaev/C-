66a35311b383707f9e54e26f0a6c852bf21e006c
NikolayKartaev <kartaevgeek@gmail.com>
2023-02-14 06:20:25 +0300

    Seminar 5 Homework 1-2

---
+++ Seminar5/Homework/1/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2*/
+
+Console.Clear();
+
+Console.Write("Введите количество элементов массива: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+int[] array = new int[N];
+
+int ArrayCount (int[] collection)
+{
+    int n = 0;
+    for (int index = 0; index < N; index++)
+    {
+        collection [index] = new Random().Next(100,1000);
+        if (collection [index]%2==0) n++;
+    }
+    return n;
+}
+
+int count = ArrayCount(array);
+
+Console.WriteLine($"Массив: [{string.Join(",", array)}]\nКоличество четных чисел: {count}");
+Console.WriteLine();
---
+++ Seminar5/Homework/2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0 */
+
+Console.Clear();
+
+Console.Write("Введите количество элементов массива: ");
+int N = int.Parse(Console.ReadLine()), sum = 0;
+Console.WriteLine();
+int[] array = new int[N];
+
+
+for (int index = 0; index < N; index ++)
+{
+    array[index] = new Random().Next(100, 1000);
+    if (index%2!=0) sum += array[index];
+}
+Console.WriteLine($"Массив: [{string.Join(",", array)}]\n Сумма чисел на нечетных позициях: {sum}");
+
+
