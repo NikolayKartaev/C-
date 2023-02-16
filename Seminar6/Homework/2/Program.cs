5405b111d23aef3b2923e4da18ab492212402862
NikolayKartaev <kartaevgeek@gmail.com>
2023-02-16 16:05:38 +0300

    Seminar 6 Homework 2

--- Seminar6/Homework/2/Program.cs
+++ Seminar6/Homework/2/Program.cs
@@ -13,7 +13,7 @@ Console.Write("Введите b2: ");
 double b2 = double.Parse(Console.ReadLine()!);
 Console.WriteLine();
 double x=(b2 - b1) / (k1 - k2), y=k1 * (b2 - b1) / (k1 - k2) + b1;
-if (k1==k2) Console.WriteLine("Прямые параллельны и не пересекаются");
+if (k1==k2) Console.WriteLine("Прямые параллельны и не пересекаются или совпадают");
 else
 {
     Console.WriteLine($"Пересечения прямых y={k1}x+{b1} и y={k2}x+{b2} состоится в точке: ({x};{y})");
