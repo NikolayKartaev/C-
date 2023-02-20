174dfa84dbd5643b522625566c813b2bef260e68
NikolayKartaev <kartaevgeek@gmail.com>
2023-02-20 07:50:14 +0300

    Seminar 8

---
+++ Seminar8/Project0/Program.cs
@@ -0,0 +1,46 @@
+﻿void FillMatrix(int[,] anyMatrix)
+{
+    for (int i = 0; i < anyMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < anyMatrix.GetLength(1); j++)
+        {
+            anyMatrix[i, j] = new Random().Next(1,21);
+        }
+    }
+}
+
+void PrintMatrix(int[,] anyMatrix)
+{
+    for (int i = 0; i < anyMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < anyMatrix.GetLength(1); j++)
+        {
+            Console.Write($"{anyMatrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите через пробел количество строк и столбцов матрицы: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+
+FillMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+
+void ChangeMatrix(int[,] mtrx)
+{
+    for (int i = 0; i < 1; i++) // цикл до 1, чтобы изменить только первую строку
+    {
+        for (int j = 0; j < mtrx.GetLength(1); j++)
+        {
+            int temp = mtrx[i, j];
+            mtrx[i, j] = mtrx[mtrx.GetLength(0) - i - 1,j]; // меняем первую и последнюю строку массива
+            mtrx[mtrx.GetLength(0) - i - 1,j] = temp;
+        }
+    }
+}
+
+ChangeMatrix (matrix);
+PrintMatrix(matrix);
---
+++ Seminar8/Project1/Program.cs
@@ -0,0 +1,52 @@
+﻿void FillMatrix(int[,] anyMatrix)
+{
+    for (int i = 0; i < anyMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < anyMatrix.GetLength(1); j++)
+        {
+            anyMatrix[i, j] = new Random().Next(1, 21);
+        }
+    }
+}
+
+void PrintMatrix(int[,] anyMatrix)
+{
+    for (int i = 0; i < anyMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < anyMatrix.GetLength(1); j++)
+        {
+            Console.Write($"{anyMatrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите через пробел количество строк и столбцов матрицы: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+while (size[0] != size[1])
+{
+    Console.Write("Вы ошиблись!\nВведите размер массива: ");
+    size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+}
+
+int[,] matrix = new int[size[0], size[1]];
+
+FillMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+
+void Transpon(int[,] anyMatrix)
+{
+    for (int i = 0; i < anyMatrix.GetLength(0); i++)
+    {
+        for (int j = i + 1; j < anyMatrix.GetLength(1); j++) // j = i+1 чтобы поменять 3 элемента (i=0, j=1, 2; i=1, j=2)
+        {
+            int temp = anyMatrix[i, j];
+            anyMatrix[i, j] = anyMatrix[j, i];
+            anyMatrix[j, i] = temp;
+        }
+    }
+}
+
+Transpon(matrix);
+PrintMatrix(matrix);
