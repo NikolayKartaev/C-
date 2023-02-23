f76ce810fe08ef7a55a4cb79eea576f67f2fffd5
NikolayKartaev <kartaevgeek@gmail.com>
2023-02-23 18:13:04 +0300

    Seminar 8 Homework 4

--- Seminar8/Homework/4/Program.cs
+++ Seminar8/Homework/4/Program.cs
@@ -6,41 +6,64 @@
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
 
-bool CheckNumber(int[] array, int number)
+bool CheckNumber(int[,,] mtrx, int number)
 {
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 0; i < mtrx.GetLength(0); i++)
     {
-        if (array[i] == number) // если такое число уже есть в массиве, вовзращаем 0
-            return false;
+        for (int j = 0; j < mtrx.GetLength(1); j++)
+        {
+            for (int k = 0; k < mtrx.GetLength(2); k++)
+            {
+                if (mtrx[i, j, k] == number)
+                    return false;
+            }
+        }
     }
-    return true; //возвращаем 1, если числа нет в массиве
+    return true;
 }
-
 
 void ThreeDArrayInput(int[,,] threeDArray)
 {
-    for (int k = 0; k < threeDArray.GetLength(0); k++)
+
+    for (int i = 0; i < threeDArray.GetLength(0); i++)
     {
-        for (int i = 0; i < threeDArray.GetLength(1); i++)
+        for (int j = 0; j < threeDArray.GetLength(1); j++)
         {
-            for (int j = 0; j < threeDArray.GetLength(2); j++)
+            for (int k = 0; k < threeDArray.GetLength(2); k++)
             {
+                int n = 10;
+                int number = new Random().Next(10, 100);
+                while (n < 90)
+                {
+                    if (CheckNumber(threeDArray, number)) threeDArray[i, j, k] = number;
+                    else
+                    {
+                        number = new Random().Next(10, 100);
+                    }
+                    n++;
+                }
 
-
-
-                    Console.Write($"{threeDArray[i, j, k]}({i}, {j}, {k})\t");
+                Console.Write($"{threeDArray[i, j, k]}({j}, {k}, {i})\t");
             }
             Console.WriteLine();
         }
     }
 }
 
+
+
+
+
 Console.Clear();
 Console.Write("Введите размер трехмерного массива через пробел: ");
 int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
 
-while (size[0]*size[1]*size[2] > end - start)
-        Console.Write("Массив такого размера");
+while (size[0] * size[1] * size[2] > 90)
+{
+    Console.Write("Массив такого размера из неповторяющихся двузначных чисел задать невозможно.\nВведите размер трехмерного массива через пробел: ");
+    size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+}
+
 int[,,] threeDimensionArray = new int[size[0], size[1], size[2]];
 
 ThreeDArrayInput(threeDimensionArray);
