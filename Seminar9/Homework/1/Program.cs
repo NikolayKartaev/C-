63036493cb909bbbf65c080d4fe25b887fac8c57
NikolayKartaev <kartaevgeek@gmail.com>
2023-02-27 12:48:53 +0300

    Seminar 9 Homework 1

---
+++ Seminar9/Homework/1/Program.cs
@@ -0,0 +1,11 @@
+﻿int FindSum(int a, int b)
+{
+    if (b == a) return b;
+    else return b + FindSum(a, b - 1);
+}
+
+Console.Write("Введите значение M: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение N: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.Write($"Сумма натуральных элементов в промежутке между {M} и {N} равна {FindSum(M, N)}");
