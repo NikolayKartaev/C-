b6114cbae85a883438138a29523765a7ab6b2c76
NikolayKartaev <kartaevgeek@gmail.com>
2023-03-22 08:32:13 +0300

    Bootcamp2

---
+++ BOOTCAMP2/1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+Console.Write("Введите количество элементов массива: ");
+int[] array = new int[int.Parse(Console.ReadLine())];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+
+Console.WriteLine(array[3]);
+// Сложность алгоритма О(1)
+
+// [5, 3, 4, 1, 2]
+// Сложность алгоритма найти сумму данного массива циклом - O(n)
+
+// [1, 2, 3, 4, 5] - Сложность алгоритма быстрой сортировки - O(n * log n)
+// ((1 + 5)/2) * 5 = 15 - Сложность алгоритма найти сумму чисел арифметической прогрессии - O(1)
+
+// O(n * log n) + O(1) или O(n * log n + 1)
+
+// Если n - большой, то n < n * log n + 1
---
+++ BOOTCAMP2/2/Program.cs
@@ -0,0 +1,28 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[,] matrix = new int[n, n];
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = (i + 1) * (j + 1);
+    }
+}
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write($"{matrix[i, j]}\t");
+    }
+    Console.WriteLine();
+}
+
+// for (int i = 0; i < n; i++)
+// {
+//     for (int j = 0; j < n; j++)
+//     {
+//         Console.Write(i*j);
+//         Console.Write(\t);
+//     }
+//     Console.WriteLine();    
+// }
