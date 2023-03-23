7608a4a73c2f4e45271ec9c33c45526764f3d6e4
NikolayKartaev <kartaevgeek@gmail.com>
2023-03-23 18:42:29 +0300

    Bootcamp1

--- BOOTCAMP1/ArrayCreator.cs
+++ BOOTCAMP1/ArrayCreator.cs
@@ -1,11 +1,22 @@
-/// 
 public static class ArrayCreator
 {
     public static int[] Create(this int n)
     {
         return new int[n];
     }
-    
 
+    public static string ConvertToStringAndPrint(this int[] array)
+    {
+        string str = $"[{String.Join(", ", array)}]";
+        System.Console.WriteLine(str);
+        return str;
+    }
 
+    public static int[] Fill(this int[] array, int min = 0, int max = 10, int seed = 0)
+    {
+        Random random = seed == 0 ? new Random() : new Random(seed);
+        // if (seed == 0) random = new Random();
+        // else random = new Random(seed);
+        return array.Select(item => random.Next(min, max)).ToArray();
+    }
 }
--- BOOTCAMP1/Program.cs
+++ BOOTCAMP1/Program.cs
@@ -1,8 +1,39 @@
-﻿// либо так:
-//using static ArrayCreator;
-//var array = Create(10);
+﻿// 1) либо так:
+using static ArrayCreator;
+using static ArrayMath;
+using System.Diagnostics;
 
-//var array = ArrayCreator.Create(10); // до добавления this в методе
+// var array = Create(10);
+
+// 2) либо так:
+// int[] array = ArrayCreator.Create(10); // до добавления this в методе
+
+// 3) либо так:
+
+int[] array = 1000000.Create()//после добавления this в методе
+                .Fill(1, 15);//после добавления this в методе
+
+//array.ConvertToStringAndPrint();
+
+int m = 10000;
+Stopwatch sw = new Stopwatch();
+sw.Start();
+
+int max = array.BadGetSum(m);
+sw.Stop();
+
+Console.WriteLine($"BadGetSum: {max} => {sw.ElapsedMilliseconds}");
 
 
-Console.WriteLine("[" + string.Join(", ", array) + "]");
+sw.Reset();
+sw.Start();
+m = array.GoodGetSum(m);
+sw.Stop();
+
+Console.WriteLine($"GoodGetSum: {max} => {sw.ElapsedMilliseconds}");
+
+
+// int[] a = Fill(array,5,6); // до добавления this в методе
+
+
+// Console.WriteLine(array.ConvertToStr());
